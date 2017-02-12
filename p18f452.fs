6d47025ffea016707bad423a4ff02b476084e00e
ivvd <ivanovdiv@gmail.com>
2017-02-12 17:08:41 +0200

    Add definitions of interrupt and timer0 registers

--- p18f452.fs
+++ p18f452.fs
@@ -9,3 +9,13 @@ $ff89 constant lata
 $ff8a constant latb
 $ff8b constant latc
 $ff8c constant latd
+
+\ INTCON registers
+$fff2 constant intcon
+$fff1 constant intcon2
+$fff0 constant intcon3
+
+\ TMR0 registers
+$ffd7 constant tmr0h
+$ffd6 constant tmr0l
+$ffd5 constant t0con
